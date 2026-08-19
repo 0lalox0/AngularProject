3081006c059da84801bd8a5c16fa60d00ed240b9
Ladislao Bordon <ladislao.bordon@gmail.com>
2026-08-18 21:41:57 -0300

    url for realbackend added

--- montesinos-landing/src/app/graphql/apollo.config.ts
+++ montesinos-landing/src/app/graphql/apollo.config.ts
@@ -3,8 +3,10 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { HttpLink } from '@apollo/client/link/http';
 
 export const apolloProvider = provideApollo(() => {
+  const urilocal = 'http://localhost:4000/graphql';
+  const urirender = 'https://montesinos-backend.onrender.com/graphql';
   const httpLink = new HttpLink({
-    uri: 'http://localhost:4000/graphql',
+    uri: urirender,
   });
 
   return new ApolloClient({
